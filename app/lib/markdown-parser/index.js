a72c37845a89189f06119c6950857cd234b40c77
stuplum <stuplum@inkfish.co>
2016-05-18 16:18:21 +0100

    @stuplum split routes into standalone files

---
+++ app/lib/markdown-parser/index.js
@@ -0,0 +1,43 @@
+'use strict';
+
+const fs = require('fs');
+
+const marked = require('marked');
+const Boom = require('boom');
+
+class MarkdownParser {
+
+    constructor(fileName) {
+        this.fileName = fileName;
+    }
+
+    parse(name, path, cb) {
+
+        fs.readFile(`${path}/${this.fileName}`, 'utf8', (err, markdown) => {
+
+            if (err) {
+                throw Boom.notFound(`${name} not found`, { stacktrace: err, from: config.APP_NAME });
+            }
+
+            cb(marked(markdown), name, path);
+        });
+
+    }
+}
+
+// function getMarkdown(type, request, componentHome, cb) {
+//
+//     const componentName = request.params.location;
+//     const componentPath = `${componentBase}/${componentName}`;
+//
+//     fs.readFile(`${componentPath}/${componentHome}`, 'utf8', (err, markdown) => {
+//
+//         if (err) {
+//             throw Boom.notFound(`${componentName} ${type} not found`, { stacktrace: err, from: config.APP_NAME });
+//         }
+//
+//         cb(marked(markdown), componentName, componentPath);
+//     });
+// };
+
+module.exports = MarkdownParser;
